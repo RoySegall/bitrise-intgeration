3060a5077b06f1a0a24eff8d75e51ee9da2ffdbc
RoySegall <roy@testim.io>
2023-03-16 07:07:56 +0200

    Init the project

---
+++ bitrise-intgeration/bitrise_intgerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitrise_intgerationApp.swift
+//  bitrise-intgeration
+//
+//  Created by Roy Segall on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct bitrise_intgerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
